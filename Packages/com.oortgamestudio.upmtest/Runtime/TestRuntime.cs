b1252dfb9e7c8ac818d21cedd585bcfd6c6eb551
oort-games <oortgamestudio@gmail.com>
2026-04-28 23:44:39 +0900

    feat : Create Upm 0.1.0

---
+++ Packages/com.oortgamestudio.upmtest/Runtime/TestRuntime.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestRuntime : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // ver 0.1.0
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
